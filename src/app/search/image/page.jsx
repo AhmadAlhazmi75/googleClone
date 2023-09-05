10cd9a1d80fadf170837b239b4aa22f46298ab81
AhmadAlhazmi75 <rregfhlty@gmail.com>
2023-09-05 06:46:04 -0700

    Add image results

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,5 +1,32 @@
-function ImageSearchPage() {
-  return <div>hello</div>;
+import ImageSearchResults from "@/components/ImageSearchResults";
+import Link from "next/link";
+
+async function ImageSearchPage({ searchParams }) {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const res = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+  const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Try searching for something else or go back to the homepage.{" "}
+        </p>
+        <Link className="text-blue-500" href="/">
+          Home
+        </Link>
+      </div>
+    );
+  }
+  return <>{results && <ImageSearchResults results={data} />}</>;
 }
 
-export default ImageSearchPage;
+export default WebSearchPage;
