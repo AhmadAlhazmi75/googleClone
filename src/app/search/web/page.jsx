1a62458524ba2af8470b8e21f12e369cb14f95bb
AhmadAlhazmi75 <rregfhlty@gmail.com>
2023-09-05 00:52:43 -0700

    handle possible errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,10 +1,29 @@
+import Link from "next/link";
+
 async function WebSearchPage({ searchParams }) {
   const res = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
   const data = await res.json();
   const results = data.items;
-  console.log(results);
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Try searching for something else or go back to the homepage.{" "}
+        </p>
+        <Link className="text-blue-500" href="/">
+          Home
+        </Link>
+      </div>
+    );
+  }
   return (
     <>
       {results?.map((result, index) => (
